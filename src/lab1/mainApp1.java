2efedb95ac8d8a925e874dce2939f78722f98e3d
paracorde <paracorde@github.com>
2025-02-17 15:52:47 +0800

    added a second test in the main function

--- src/lab1/mainApp1.java
+++ src/lab1/mainApp1.java
@@ -4,10 +4,17 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        int c = 3;
+        int m = 10;
+
         System.out.println("Welcome to Scientific Calculator!");
         System.out.println("Program started ...");
-        System.out.println(b + " to power " + n + " = " + myLibrary.Power(b,n));
+        System.out.println(b + " to power " + n + " = " + myLibrary.Power(c, m));
         System.out.println(n + "! = " + myLibrary.factorial(n));
+
+        System.out.println(c + " to power " + m + " = " + myLibrary.Power(c, m));
+        System.out.println(m + "! = " + myLibrary.factorial(m));
+
         System.out.println("Program ..Ended ...");
     }
 }
